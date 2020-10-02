2ae4e4b4643e5fb52f775a16298b4e29ca2b5589
Durin101 <60183871+AlfredoThill@users.noreply.github.com>
2020-10-01 21:11:21 -0500

    Update excercise2.js

--- excercise2.js
+++ excercise2.js
@@ -1,18 +1,23 @@
-// exercise 2, create http service with a single post using express
+// exercise 2, create http service with a single post
 
+// Since libraries are allowed I'm using express and body-parser. On a personal note, it's been a few months since I coded, 
+// because of moving to a different country and the loong cuarentine in Argentina. I did have trouble with this excercise, ended up sending the json through Ajax
+// but I didn't got the code to work, I have used ajax in the past and I am very familiar with http, please check my other repo: https://github.com/AlfredoThill/durin.monsters.
+
+// setting up the modules and the app
 const express = require("express"); 
 const bodyParser = require("body-parser") 
-  
 const app = express(); 
 app.use(bodyParser.urlencoded({ 
     extended:true
 })); 
-  
+
+
 app.get("/", function(req, res) { 
   res.sendFile(__dirname + "/excercise2.html"); 
-//  res.send({ phrase: "mi frase"})
 }); 
-  
+
+// The post
 app.post("/palindrome", function(req, res) { 
   const phrase = req.body["phrase"]; 
   const boolean = palindrome(phrase);  
@@ -25,6 +30,7 @@ app.listen(3000, function(){
   console.log("server is running on port 3000"); 
 }) 
 
+// Auxiliar function with regular expresions to check for palindrome
 function palindrome(str) {
     const re = /[\W_]/g;
     const lowRegStr = str.toLowerCase().replace(re, '');
